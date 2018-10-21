4b78539b80d52a4b3ccea5884603be4c328ed49e
Nicolas Darr <nicolas.darr96@gmail.com>
2018-10-21 18:30:58 +0200

    Tweaked

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 import numpy as np
 
+#disable ssl warnings for better readability
+import urllib3
+urllib3.disable_warnings()
+
 #Auslesen der Programmparameter
 type = sys.argv[1]
 url = sys.argv[2]
@@ -30,9 +34,15 @@ for i in range(iterations):
     t_diff = t1 - t0
     elapsed_ms = int(((t_diff.days * 86400000) + (t_diff.seconds * 1000) + (t_diff.microseconds / 1000)) + 0.5)
     times[i] = elapsed_ms
-    print("Request " + str(i+1) + ":\t" + str(elapsed_ms) + "ms\t" + " - " + str(resp.status_code))
+    print(type + " request " + str(i+1) + ":\t" + str(elapsed_ms) + "ms\t" + " - " + str(resp.status_code))
 
 print(times)
+print(type + " Cold: " + str(times[0]))
+print(type + " Median: " + str(np.median(times)) + "ms")
+print(type + " Mean: " + str(np.mean(times)) + "ms")
+print(type + " Max: " + str(np.max(times)) + "ms")
+print(type + " Min: " + str(np.min(times)) + "ms")
+
 plt.plot(np.arange(1, iterations+1, dtype=np.int32), times)
 plt.axis([1, iterations+1, np.min(times)*0.9, np.max(times)*1.1])
 plt.title(type)
